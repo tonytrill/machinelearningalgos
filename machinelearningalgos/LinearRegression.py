bb5545a00f69895c25cd95bbc889233e669a50de
Tony Silva <tsilva@tax-guard.local>
2019-07-09 14:49:44 -0600

    finished closed form solution

--- machinelearningalgos/LinearRegression.py
+++ machinelearningalgos/LinearRegression.py
@@ -4,7 +4,7 @@ from numpy import random
 class LinearRegression:
     def __init__(self, epochs, learning_rate, verbose):
         self.w = []
-        self.b = []
+        self.b = 0
         self.epochs = epochs
         self.alpha = learning_rate
         if verbose == None:
@@ -16,7 +16,7 @@ class LinearRegression:
     Get the predicition of the linear regression model
     """
     def predict(self,x):
-        return(np.dot(self.w,x) + self.b)
+        return(np.dot(x,self.w) + self.b)
     
     """
     Calculate the Mean Squared Error of two vectors.
@@ -30,13 +30,13 @@ class LinearRegression:
     Calculate the gradient of the Mean Squared Error with respect to w (the weights)
     """
     def gradient_w(self, x, y):
-        return np.sum(np.dot(-2*x,(y-(np.dot(self.w, x) + self.b))))*(1/len(y))
+        return (np.dot((y-(np.dot(x, self.w) + self.b)),-2*x)).mean()
 
     """
     Calculate the gradient of the Mean Squared Error with respect to b (the intercept value)
     """
     def gradient_b(self, x, y):
-        return np.sum(-2*(y-(np.dot(self.w, x) + self.b)))*(1/len(y))
+        return (-2*(y-(np.dot(x,self.w) + self.b))).mean()
 
     """
     Update the weights by adding the gradient of the loss function and the learning rate (alpha)
@@ -56,24 +56,34 @@ class LinearRegression:
     """
     def fit(self, x, y):
         # Initialize 0 weights and intercept as the number of columns in the data
-        self.w = np.zeros(len(x))
-        self.b = np.zeros(len(x))
+        self.w = np.zeros(x.shape[1])
         for epoch in range(self.epochs):
             self.update_weights(x,y)
+            print(self.w, self.b)
             self.update_intercept(x,y)
             if self.verbose == True and epoch % 10 == 0:
                 print("epoch number: " + str(epoch) + " ran!")
                 print("MSE: " + str(self.mse(y, self.predict(x))))
+    
+    """
+    This is the closed form solution of Linear Regression.
+    y = wx
+    where x_0 = 1 for all instances.
+    """
+    def closed_form_solution(self, x, y):
+        # Append a column of 1's to the data in order to get the intercept value!
+        intercept = np.ones(shape=(x.shape[0], 1))
+        x = np.append(intercept, x, axis=1)
+        return np.linalg.inv(x.T @ x) @ (x.T @ y)
 
 
 if __name__ == "__main__":
     # Generate some fake data to pass into the learner
-    x1 = 3*random.normal(size=100)
-    x2 = 10*random.normal(size=100)
-    x3 = random.normal(size=100)
-    x = np.array([x1,x2,x3])
-    y = 2 + x1 + x2  + x3
-    print(y)
-    test = LinearRegression(epochs=50, learning_rate=0.2, verbose=True)
-    test.fit(x,y)
-    print(test.w, test.b)
+    np.random.seed(0)
+    x = np.random.rand(100, 4)
+    y = 2 * x[...,0] + 3 * x[...,1] +  + 4 * x[...,2] + x[...,3] + 2
+    y.shape = (y.shape[0], 1)
+    random.seed(123)
+    test = LinearRegression(epochs=10, learning_rate=.1, verbose=True)
+    print(test.closed_form_solution(x,y))
+    #print(test.fit(x,y))
